dc00833f3574527e9119bc1958ea96a3a6b1379a
Nicholas Simpson <nicksimpson@Nicholass-MacBook-Pro.local>
2020-11-18 18:54:09 +0000

    Fail Test

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/patients"}, loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
@@ -15,6 +16,8 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ;
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write(reqBody +". Thank you client");
     }
 }
