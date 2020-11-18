cae4b8b93e8fedfedc218e8bef571310198f93d4
Nicholas Simpson <nicksimpson@Nicholass-MacBook-Pro.local>
2020-11-18 17:39:30 +0000

    Bold that shit

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -10,7 +10,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello, world!");
+        resp.getWriter().write("<b>Hello, world!</b>");
     }
 
     @Override
